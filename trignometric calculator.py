60a07588b697e79c5fded51739218f3d773db8a5
Simranshar <simransharma292000@gmail.com>
2022-04-28 13:14:52 +0530

    Initial commit

---
+++ trignometric calculator.py
@@ -0,0 +1,8 @@
+from math import *
+a=int(input("a"))
+b=int(input("b"))
+x = sin(a) + sin(b)
+print(x)
+while(True):
+    num1=int(input("enter number "))
+    operator = input("tell the operator you want to use")
